102247daba0c86c241acf2d8676c604e3cde3d96
soshan <soshanqu@163.com>
2016-05-10 10:42:29 +0800

    字符串
    添加字符串截取

---
+++ ms_string.hpp
@@ -0,0 +1,26 @@
+#ifndef MS_STRING_HPP_20160307_
+#define MS_STRING_HPP_20160307_
+
+#include <string>
+
+namespace ms_string {
+	// T is std::vector|std::list|std::set
+	template <class T>
+	bool SplitString2Container(const std::string& content, char ch, T& container) {
+		container.clear();
+		if (content.empty()) return true;
+		long l_index = -1, l_last;
+		while ((l_index = content.find(ch, l_last = (l_index + 1))) > 0) {
+			typename T::iterator iter(container.end());
+			container.insert(iter, content.substr(l_last, l_index - l_last));
+		}
+		if (l_last >= 0) {
+			typename T::iterator iter(container.end());
+			container.insert(iter, content.substr(l_last));
+		}
+		return true;
+	}
+	
+} // namespace ms_string
+
+#endif
